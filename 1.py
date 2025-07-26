9931aa9aadf8f5620457f4a885f37f6b233f560c
Abdullah <as37755488@gmail.com>
2025-07-27 01:38:28 +0300

    Add first program

---
+++ 1.py
@@ -0,0 +1,18 @@
+from math import pi
+
+def main():
+    radius = get_radius()
+    area = pi * radius ** 2
+    print(f"The area of the circle with radius {radius} is: {area:.2f}")
+
+def get_radius():
+    while True:
+        radius = input("Enter the radius of the circle: ")
+
+        if radius.isdigit():
+            return float(radius)
+        
+        print("Invalid input. Please enter a valid number.")
+        
+if __name__ == "__main__":
+    main()
