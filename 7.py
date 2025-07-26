31b72ef63f172e9c6ab9297c1b4c397de97db1f7
Abdullah <as37755488@gmail.com>
2025-07-27 02:21:40 +0300

    Add the seventh program

---
+++ 7.py
@@ -0,0 +1,18 @@
+def main():
+    upper_bound = get_upper_bound()
+    
+    for i in range(2, upper_bound + 1, 2):
+        print(i)
+
+def get_upper_bound():
+    while True:
+        number = input("Enter the upper bound of the even numbers: ")
+
+        if number.isdigit():
+            return int(number)
+        
+        print("Invalid input. Please enter a valid number.")
+
+
+if __name__ == "__main__":
+    main()
